104ccbb5aceb6e89c710a171aa0a8dd259f5864d
apiwatdev <123138365+apiwasooscg@users.noreply.github.com>
2023-07-05 23:00:08 +0700

    setup project

---
+++ backend/src/main/java/com/apiwatdev/rida_service/RidaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apiwatdev.rida_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidaServiceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/apiwatdev/rida_service/RidaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiwatdev.rida_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
